449ce5297a7d73d4012231f0dedfc07bc855d83c
OjBilli <133760903+OjBilli@users.noreply.github.com>
2024-04-24 15:17:03 +0100

    new

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+import './bootstrap';
+
+import 'admin-lte/plugins/jquery/jquery.min.js';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js'
+import 'admin-lte/dist/js/adminlte.min.js'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Routes from './routes.js';
+
+const app = createApp({});
+
+
+const router = createRouter({
+    routes: Routes,
+    history: createWebHistory(),
+});
+
+
+app.use(router);
+
+
+app.mount('#app');
+
---
+++ resources/js/routes.js
@@ -0,0 +1,9 @@
+import Dashboard from "./components/Dashboard.vue";
+
+export default[
+    {
+        path: '/admin/dashboard',
+        name: 'admin.dashboard',
+        component: Dashboard,
+    }
+]
