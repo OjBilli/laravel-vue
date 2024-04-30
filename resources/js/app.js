569bef2df76b270d88485d69ecc9b0e15b7b0850
OjBilli <133760903+OjBilli@users.noreply.github.com>
2024-04-30 20:09:14 +0100

    Vue pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import './bootstrap';
 import 'admin-lte/plugins/jquery/jquery.min.js';
 import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js'
 import 'admin-lte/dist/js/adminlte.min.js'
-import { createApp } from 'vue';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Routes from './routes.js';
